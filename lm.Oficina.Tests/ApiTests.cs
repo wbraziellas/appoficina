1d79c349eaa0b0ee0f5dcc142af24933a9a3ea09
William <William@WILLIAM-PC>
2017-01-05 00:24:24 +0000

    Inserção da classe de teste.

---
+++ lm.Oficina.Tests/ApiTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lm.Oficina.Domain;
+using System.Linq;
+
+namespace lm.Oficina.Tests
+{
+    [TestClass()]
+    public class ApiTests
+    {
+        #region Propriedades
+        private ConexaoParadox _conexaoParadox;
+        private ConexaoParadox conexaoParadox
+        {
+            get { return _conexaoParadox ?? (_conexaoParadox = new ConexaoParadox()); }
+        }
+        #endregion
+
+        [TestMethod()]
+        public void Testar_Conexao_Paradox()
+        {
+            var conexao = conexaoParadox.Conectar();
+            Assert.IsFalse(conexao, "Não conectou à base!");
+        }
+
+    }
+}

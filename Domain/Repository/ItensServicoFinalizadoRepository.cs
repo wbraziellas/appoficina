46e60fcba8ed5d7f70dc83e13057fba22b9bbe5c
William <William@WILLIAM-PC>
2017-01-13 00:49:29 +0000

    Incremeto de Repository ServicoFinalizado

---
+++ Domain/Repository/ItensServicoFinalizadoRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lm.Oficina.Domain;
+using lm.Oficina.DTO;
+using System.Data.Odbc;
+using System.Data;
+
+namespace lm.Oficina.Domain.Repository
+{
+    class ItensServicoFinalizadoRepository
+    {
+        #region Propriedades
+
+        private string _strSql = string.Empty;
+
+        private ConexaoParadox _conexao;
+        private ConexaoParadox conexao
+        {
+            get { return _conexao ?? (_conexao = new ConexaoParadox()); }
+        }
+
+        #endregion
+
+        public void GravarItensServicoFinalizado(ItensServicoFinalizadoDTO ItensServicoFinalizado)
+        {
+            conexao.Conectar();
+
+            try
+            {
+
+                _strSql = "INSERT" +
+                                "INTO" +
+                          "SERVICOFINALIZADO(CODIGOSERVICOFINALIZADO, CODIGOOSFINALIZADA, DESCRICAODOSERVICO, SERVICOFINALIZADO)" +
+                          "VALUES(@CODIGOSERVICOFINALIZADO, @CODIGOOSFINALIZADA, @DESCRICAODOSERVICO, @SERVICOFINALIZADO)";
+
+                OdbcCommand _sqlCmd = new OdbcCommand(_strSql);
+                _sqlCmd.Parameters.Add(new OdbcParameter("@CODIGOSERVICOFINALIZADO", ItensServicoFinalizado.CodigoServicoFinalizado));
+                _sqlCmd.Parameters.Add(new OdbcParameter("@CODIGOOSFINALIZADA", ItensServicoFinalizado.CodigoOsFinalizada));
+                _sqlCmd.Parameters.Add(new OdbcParameter("@DESCRICAOSERVICO", ItensServicoFinalizado.DescricaoDoServico));
+                _sqlCmd.Parameters.Add(new OdbcParameter("@SERVICOFINALIZADO", ItensServicoFinalizado.ServicoRealizado));
+
+                _sqlCmd.ExecuteNonQuery();
+            }
+            catch(Exception eError)
+            {
+                eError.GetBaseException();
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        public List<ItensServicoFinalizadoDTO> SelecionarItensServicoFinalizado(int IdServico)
+        {
+            conexao.Conectar();
+
+            _strSql = "SELECT" +
+                            "CODIGOSERVICOREALIZADO" +
+                            "CODIGOOSFINALIZADA" +
+                            "DESCRICAODOSERVICO" +
+                            "SERVICOREALIZADO" +
+                       "FROM ITENSSERVICOFINALIZADO" + 
+                       "WHERE" +
+                             "CODIGOOSFINALIZADA = @CODIGOOSFINALIZADA";
+
+            OdbcCommand _strCmd = new OdbcCommand(_strSql);
+            _strCmd.Parameters.Add(new OdbcParameter("@CODIGOOSFINALIZADA", IdServico));
+
+            var adapter = new OdbcDataAdapter() { SelectCommand = _strCmd };
+            var data = new DataTable();
+
+            adapter.Fill(data);
+
+            return ConverterDataEmItensServicoFinalizadoDTO(data);            
+        }
+
+        #region Métodos Privados
+
+        private List<ItensServicoFinalizadoDTO> ConverterDataEmItensServicoFinalizadoDTO(DataTable data)
+        {
+            var listaItens = new List<ItensServicoFinalizadoDTO>();
+
+            foreach(DataRow row in data.Rows)
+            {
+                var Item = new ItensServicoFinalizadoDTO();
+                var linha = row.ItemArray;
+
+                Item.CodigoServicoFinalizado = int.Parse(linha[0].ToString());
+                Item.CodigoOsFinalizada = int.Parse(linha[1].ToString());
+                Item.DescricaoDoServico = linha[2].ToString();
+                Item.ServicoRealizado = bool.Parse(linha[3].ToString());
+
+                listaItens.Add(Item);
+            }
+
+            return listaItens;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Domain/Repository/ServicoFinalizadoRepository.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lm.Oficina.Domain;
+using lm.Oficina.DTO;
+using System.Data.Odbc;
+using System.Data;
+
+namespace lm.Oficina.Domain.Repository
+{
+    public class ServicoFinalizadoRepository
+    {
+        #region propriedades
+
+        string _strSql = string.Empty;
+
+        private ConexaoParadox _conexao;
+        private ConexaoParadox conexao
+        {
+            get { return _conexao ?? (_conexao = new ConexaoParadox()); }
+        }
+
+        #endregion
+
+        public void GravarServicoFinalizado(ServicoFinalizadoDTO servicoFinalizadoDto)
+        {
+            conexao.Conectar();
+
+            try
+            {
+                _strSql = "INSERT INTO SERVICOFINALIZADO" +
+                                "(CODIGODAOS, NOMECLIENTE, PLACAVEICULO, DATAOS, DATAOSFINALIZADA, MECANICOOS, CODIGOSERVICOFEITO)" +
+                                "VALUES(@CODIGODAOS, @NOMECLIENTE, @PLACAVEICULO, @DATAOS, @DATAOSFINALIZADA, @MECANICOOS, @CODIGOSERVICOFEITO)";
+
+                OdbcCommand _sqlCmd = new OdbcCommand(_strSql);
+                _sqlCmd.Parameters.Add(new OdbcParameter("@CODIGODAOS", servicoFinalizadoDto.CodigoDaOS));
+                _sqlCmd.Parameters.Add(new OdbcParameter("@NOMECLIENTE", servicoFinalizadoDto.NomeCliente));
+                _sqlCmd.Parameters.Add(new OdbcParameter("@PLACAVEICULO", servicoFinalizadoDto.PlacaVeiculo));
+                _sqlCmd.Parameters.Add(new OdbcParameter("@DATAOS", servicoFinalizadoDto.DataOs));
+                _sqlCmd.Parameters.Add(new OdbcParameter("@DATAOSFINALIZADA", servicoFinalizadoDto.DataOsFinalizada));
+                _sqlCmd.Parameters.Add(new OdbcParameter("@MECANICOOS", servicoFinalizadoDto.MecanicoOs));
+                _sqlCmd.Parameters.Add(new OdbcParameter("@CODIGOSERVICOFEITO", servicoFinalizadoDto.CodigoServicoFeito));
+
+                _sqlCmd.ExecuteNonQuery();
+
+
+            }
+            catch(Exception eError)
+            {
+                eError.GetBaseException();
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }       
+
+        }
+
+        public List<ServicoFinalizadoDTO> SelecionarServicoFinalizado()
+        {
+            _strSql = "SELECT" +
+                            "CODIGODAOS" +
+                            "NOMECLIENTE" +
+                            "PLACAVEICULO" +
+                            "DATAOS" +
+                            "DATAOSFINALIZADA" +
+                            "MECANICOOS" +
+                            "CODIGOSERVICOFEITO" +
+                       "FROM SERVICOFINALIZADO";
+
+            conexao.Conectar();
+
+            try
+            {
+                OdbcCommand _strCmd = new OdbcCommand(_strSql);
+
+                var adapter = new OdbcDataAdapter() { SelectCommand = _strCmd };
+                var data = new DataTable();
+
+                adapter.Fill(data);
+                return ConverterDataEmServicoFinalizado(data);
+
+            }
+            catch(Exception eError)
+            {
+                return new List<ServicoFinalizadoDTO>();
+            }
+            finally
+            {
+                conexao.Desconectar();
+            }
+        }
+
+        #region Métodos Privados
+
+        private List<ServicoFinalizadoDTO> ConverterDataEmServicoFinalizado(DataTable data)
+        {
+            var listaServico = new List<ServicoFinalizadoDTO>();
+
+            foreach(DataRow row in data.Rows)
+            {
+                var servico = new ServicoFinalizadoDTO();
+                var linha = row.ItemArray;
+
+                servico.CodigoDaOS = int.Parse(linha[0].ToString());
+                servico.NomeCliente = linha[1].ToString();
+                servico.PlacaVeiculo = linha[2].ToString();
+                servico.DataOs = DateTime.Parse(linha[3].ToString());
+                servico.DataOsFinalizada = DateTime.Parse(linha[4].ToString());
+                servico.MecanicoOs = linha[5].ToString();
+                servico.CodigoServicoFeito = int.Parse(linha[6].ToString());
+
+                listaServico.Add(servico);
+            }
+
+            return listaServico;
+        }
+
+        #endregion
+    }
+}
